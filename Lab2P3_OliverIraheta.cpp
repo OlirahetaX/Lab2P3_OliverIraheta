6b17ead52de52d23d90609d2c4c1f8b62a033362
OlirahetaX <oliveriraheta57@gmail.com>
2023-04-28 14:53:46 -0600

    Segundo commit

--- Lab2P3_OliverIraheta.cpp
+++ Lab2P3_OliverIraheta.cpp
@@ -9,9 +9,21 @@ using namespace std;
 
 int main() {
 	int salir = 0;
-	array<int, 5> values;
+	array<char, 10> values;
+	srand(time(NULL));
 	do {
-		cout << "1- Ordenamiento y omisión en arreglos" << endl << "2- Factoriales y figuras" << endl << "0- SALIR" << endl << "-> ";
+		
+		cout << "\n1- Ordenamiento y omisión en arreglos" << endl << "2- Factoriales y figuras" << endl << "0- SALIR" << endl << "-> ";
 		cin >> salir;
+		if (salir == 1) {
+			for (int i = 0; i < 10; i++)
+				values[i] = 97 + rand() % (123 - 97);
+			cout << "Arreglo original: ";
+			for (int i = 0; i < 10; i++)
+				cout << values[i] << " ";
+		}
+		else if (salir == 2) {
 
+		}
 	} while (salir);
+}
